d2d6627c26a344ba912a975b0545a11c023d0e84
Syed <nizar.syed@cgi.com>
2018-06-14 17:45:01 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-
+import { CityComponent } from './city/city.component';
+import { HttpModule } from '@angular/http';
+import { CityService } from './_shared/city.service';
+import { enableProdMode } from '@angular/core';
+import { WeatherComponent } from './weather/weather.component';
+import { WeatherService } from './_shared/weather.service';
+enableProdMode();
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CityComponent,
+    WeatherComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [CityService, WeatherService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
